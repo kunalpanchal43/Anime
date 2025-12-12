883465ec7c09e388ebb5b6c23408ff780dd29d6d
Kunal Panchal <kunalpanchal077@gmail.com>
2025-12-12 22:36:02 +0530

    Switch to HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import AnimeItem from "./Components/AnimeItem";
 import Gallery from "./Components/Gallery";
 import Homepage from "./Components/Homepage";
 
 function App() {
   return (
-    <BrowserRouter basename="/Anime">
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/anime/:id" element={<AnimeItem />} />
         <Route path="/character/:id" element={<Gallery />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
